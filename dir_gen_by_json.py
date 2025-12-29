bf8beaaccbe7eab351b6ceaba5b8fe8e65402c26
MyGitcode <MyGitcode@qq.com>
2025-12-29 19:51:28 +0800

    更新.py文件头信息

--- dir_gen_by_json.py
+++ dir_gen_by_json.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Copyright © 2025 码上工坊（微信公众号，头条号同名）
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Module/Script Name: dir_gen.py
+# Author: 码上工坊 (微信公众号，头条号同名）
+# Created: 2025-05
+# Description: 目录结构生成器脚本（配置文件使用JSON格式）。
+
 import argparse
 import json
 import sys
